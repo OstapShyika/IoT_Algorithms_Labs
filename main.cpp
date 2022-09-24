f444b40e1fa39e6358e5b343dd177e65b8cc6b01
OstapShyika <92269764+OstapShyika@users.noreply.github.com>
2022-09-24 16:07:00 +0300

    Added lab-2 code

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+struct Cell {
+    int x, y, distance;
+    Cell(int x, int y, int distance) {
+        this->x = x;
+        this->y = y;
+        this->distance = distance;
+    }
+};
+class Maze {
+public:
+    const vector<pair<int, int>> DIR = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
+    int shortest_path(vector<vector<int>> matrix, pair<int, int> start, pair<int, int> end) {
+        if (matrix[start.first][start.second] == 0 || matrix[end.first][end.second] == 0) {
+            return -1;
+        }
+        int r = matrix.size();
+        int c = matrix[0].size();
+        queue<Cell> q;
+        q.emplace(Cell(start.first, start.second, 0));
+
+        while (!q.empty()) {
+            Cell curr = q.front();
+            if (curr.x == end.first && curr.y == end.second) {
+                return curr.distance;
+            }
+            q.pop();
+            matrix[curr.x][curr.y] = 0;
+            for (auto& x: DIR) {
+                int nr = curr.x + x.first;
+                int nc = curr.y + x.second;
+                if (nr >= 0 && nr < r && nc >= 0 && nc < c && matrix[nr][nc] != 0) {
+                    matrix[nr][nc] = 0;
+                    q.emplace(Cell(nr, nc, curr.distance + 1));
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    Maze maze;
+    vector<vector<int>> m =
+            {
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+                    { 1, 0, 1, 0, 1, 1, 1, 0, 1, 1 },
+                    { 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
+                    { 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+                    { 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 },
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 },
+                    { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                    { 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+                    { 1, 1, 0, 0, 0, 0, 1, 0, 0, 1 }
+            };
+    pair<int, int> s = {0, 0}, e = {3, 4};
+    cout << maze.shortest_path(m, s, e);
+    return 0;
+}
